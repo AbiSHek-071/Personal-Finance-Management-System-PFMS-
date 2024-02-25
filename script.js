e2bc8eec0afa50db96ed653b3cf5e5630d34bd92
Abhishek <abhishekpawoor654@gmail.com>
2024-02-24 23:57:23 -0800

    front end ready

--- script.js
+++ script.js
@@ -3,24 +3,51 @@ const navgrow = document.querySelector(".nav_grow");
 const navshot = document.querySelector(".nav_short");
 const backbtn = document.querySelector(".back_arrow");
 const dashboard = document.querySelector(".container");
+const taskbar = document.querySelector(".taskbar");
+
 console.log(backbtn);
 
 function navigation(){
-    navgrow.style.display = "flex";
-   
-    dashboard.classList.add("container_ease_in");
-    navgrow.classList.add("nav_ease_in");
-    navshot.classList.add("navshot_animation");
+    if(navgrow.classList.contains("navgrow_anm")){
+        navgrow.classList.remove("navgrow_anm");
+      
+        navgrow.style.display = "none";
+    }
+    else{
+        navgrow.classList.add("navgrow_anm");
+        dashboard.classList.add("container_ease_in");
+        taskbar.classList.add("container_ease_in");
+        navgrow.style.display = "flex";
+    }
  
     
 
 }
 function shrink(){
-    navgrow.style.display = "none";
-    dashboard.classList.remove("container_ease_in");
-    navshot.classList.remove("navshot_animation");
+    if(navgrow.classList.contains("navgrow_anm")){
+        navgrow.classList.remove("navgrow_anm");
+        dashboard.classList.remove("container_ease_in");
+        taskbar.classList.remove("container_ease_in");
+       
+    }
+    else{
+        navgrow.classList.add("navgrow_anm");
+        navgrow.style.display = "flex";
+    }
    
 }
 
 menu.addEventListener("click",navigation);
 backbtn.addEventListener("click",shrink);
+
+
+
+
+
+
+
+
+
+
+
+
