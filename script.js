9c197edffaaa5d47976f14e83b9ffb40a10112de
Abhishek <abhishekpawoor654@gmail.com>
2024-03-24 02:50:38 -0700

    FINAL

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+// --------Navigation--------
+
+
 const menu = document.querySelector("#menu");
+const head = document.querySelector(".head");
 const navgrow = document.querySelector(".nav_grow");
 const navshot = document.querySelector(".nav_short");
 const backbtn = document.querySelector(".back_arrow");
 const dashboard = document.querySelector(".container");
 const taskbar = document.querySelector(".taskbar");
 
-console.log(backbtn);
+menu.addEventListener("click",navigation);
+backbtn.addEventListener("click",shrink);
 
 function navigation(){
     if(navgrow.classList.contains("navgrow_anm")){
@@ -37,17 +42,68 @@ function shrink(){
    
 }
 
-menu.addEventListener("click",navigation);
-backbtn.addEventListener("click",shrink);
+// ---------Dashboard_Controls------
 
 
+// ----wallet_openerAndclose--------
+
+const exit = document.querySelector("#exit");
+const innerWallet = document.querySelector(".container3");
+const outerWallet = document.querySelector(".wallet");
 
 
+        innerWallet.addEventListener("click",()=>{
+            outerWallet.classList.add("wallet_open");
+            console.log("doned");
+        });
+        exit.addEventListener("click", (event) => {
+    event.stopPropagation(); // Prevent click event from bubbling up
+    outerWallet.classList.remove("wallet_open");
+
+  
+    console.log("done close");
+});
+
+// --------fgoals_openerAndcloser--------
+
+const innerFgoals = document.querySelector(".container4");
+const outerFgoals = document.querySelector(".fgoals");
+const fExit = document.querySelector("#exit_fgoals");
+
+        innerFgoals.addEventListener("click",()=>{
+            outerFgoals.classList.add("fgoal_open");
+            console.log(outerFgoals);
+        });
+        fExit.addEventListener("click",(e)=>{
+            e.stopPropagation();
+            outerFgoals.classList.remove("fgoal_open");
+        });
 
 
+// -------------graph------------------
+
+ const outerGraph = document.querySelector(".container7");
+ const innerGraph = document.querySelector(".graphs");
+ const gExit = document.querySelector("#exit_graph");
+
+        outerGraph.addEventListener("click",()=>{
+           innerGraph.classList.add("graph_open") ;
+        });
+        gExit.addEventListener("click",(e)=>{
+            e.stopPropagation();
+            innerGraph.classList.remove("graph_open") ;
+        });
 
 
+// ----------------payables---------------
+const outerPay = document.querySelector(".container8");
+const innerPay = document.querySelector(".payabls");
+const pExit = document.querySelector("#exit_pay");
 
-
-
-
+outerPay.addEventListener("click",()=>{
+    innerPay.classList.add("pay_open") ;
+ });
+ pExit.addEventListener("click",(e)=>{
+     e.stopPropagation();
+     innerPay.classList.remove("pay_open") ;
+ });
